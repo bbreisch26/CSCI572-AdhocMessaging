de401e96f44269141da81472312ecae870b3d4d9
bbreisch26 <43624702+bbreisch26@users.noreply.github.com>
2023-10-30 14:13:37 -0600

    Added adhocmessaging project skeleton

---
+++ adhocmessaging/app/src/main/java/com/csci572/adhocmessaging/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.csci572.adhocmessaging
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.csci572.adhocmessaging.ui.theme.AdhocmessagingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AdhocmessagingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AdhocmessagingTheme {
+        Greeting("Android")
+    }
+}
