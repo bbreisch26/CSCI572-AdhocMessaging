081a2f45a544424458b5ca5f0c8ca29f2a17dbf8
Michael Alvarez <maalvarez@mymail.mines.edu>
2023-11-21 12:06:02 -0700

    Clean up MainActivity and set tup reciever again

--- adhocmessaging/app/src/main/java/com/csci572/adhocmessaging/WiFiDirectBroadcastReceiver.kt
+++ adhocmessaging/app/src/main/java/com/csci572/adhocmessaging/WiFiDirectBroadcastReceiver.kt
@@ -3,11 +3,13 @@ package com.csci572.adhocmessaging
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.net.wifi.p2p.WifiP2pDevice
-import android.net.wifi.p2p.WifiP2pDeviceList
+import android.net.NetworkInfo
+import android.net.wifi.p2p.WifiP2pInfo
 import android.net.wifi.p2p.WifiP2pManager
+import android.net.wifi.p2p.WifiP2pManager.ConnectionInfoListener
 import android.net.wifi.p2p.WifiP2pManager.PeerListListener
 import android.util.Log
+
 
 /**
  * A BroadcastReceiver that notifies of important Wi-Fi p2p events.
@@ -15,8 +17,7 @@ import android.util.Log
 class WiFiDirectBroadcastReceiver(
     private val manager: WifiP2pManager?,
     private val channel: WifiP2pManager.Channel,
-    private val activity: MainActivity,
-    private val peerListListener: PeerListListener
+    private val activity: MainActivity
 ) : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
@@ -38,10 +39,18 @@ class WiFiDirectBroadcastReceiver(
             WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
                 // Call WifiP2pManager.requestPeers() to get a list of current peers
                 Log.v("BroadcastReceiver", "Called P2P PEERS CHANGED ACTION")
-                manager?.requestPeers(channel, peerListListener)
+                manager?.requestPeers(channel, WifiP2pManager.PeerListListener { peers ->
+                    // Perform action with peers
+                    // May not be applicable now that we are using service discovery
+                    activity.peerList = peers
+                })
             }
             WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
                 // Respond to new connection or disconnections
+                Log.v("BroadCast", "WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION")
+                WifiP2pManager.EXTRA_NETWORK_INFO
+
+                val networkP2pInfo: WifiP2pInfo? = intent.getParcelableExtra(WifiP2pManager.EXTRA_WIFI_P2P_INFO)
             }
             WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
                 // Respond to this device's wifi state changing
